797f844b18c99a2270f41596b81a382f787c46af
Nipun Jonnalagadda <coolnipunj@gmail.com>
2021-02-21 16:10:19 -0500

    add ons

---
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/STest.java
@@ -0,0 +1,448 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+
+
+@TeleOp(name="LServo", group="BionicBot")
+public class STest extends LinearOpMode {
+    BNO055IMU imu;
+
+    Orientation angles;
+    Orientation newAngles;
+    LaunchServoTest robot = new LaunchServoTest();   // Use a Pushbot's hardware
+    private ElapsedTime runtime = new ElapsedTime();
+
+    static final double COUNTS_PER_MOTOR_REV = 537.6;    // eg: TETRIX Motor Encoder
+    static final double DRIVE_GEAR_REDUCTION = 1.0;     // This is < 1.0 if geared UP
+    static final double WHEEL_DIAMETER_INCHES = 4.0;     // For figuring circumference
+    static final double CIRCUMFERENCE = WHEEL_DIAMETER_INCHES * Math.PI;
+    static final double COUNTS_PER_INCH = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
+            (CIRCUMFERENCE);
+    static final double DRIVE_SPEED = 0.6;
+    static final double TURN_SPEED = 0.5;
+
+    @Override
+    public void runOpMode() {
+        robot.init(hardwareMap);
+
+        // Send telemetry message to signify robot waiting;
+        telemetry.addData("Status", "Resetting Encoders");    //
+        telemetry.update();
+
+       /* robot.leftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.rightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.leftBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.rightBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+
+
+
+        telemetry.addData("Path0",  "Starting at %7d :%7d",
+                robot.leftDrive.getCurrentPosition(),
+                robot.rightDrive.getCurrentPosition(),
+                robot.leftBack.getCurrentPosition(),
+                robot.rightBack.getCurrentPosition());
+        telemetry.update();
+        robot.wobblyClaw.setPosition(-1);*/
+
+       //robot.shootSlider.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+    waitForStart();
+        while (opModeIsActive())
+        {
+        //robot.rotServo.setPower(1);
+       // robot.LServo.setPosition(0.7);
+
+
+            TurnShootSlider();
+
+        //DriveForwardDistance(.5,12);
+        //StrafeLeftDistance(.5,14);
+    /*DriveForwardDistance(.5,92);
+    wobblyJoint(.2,-600);
+    robot.wobblyClaw.setPosition(.8);
+    DriveBackwardDistance(.5,46);
+    StrafeRightDistance(.5,23);
+    TurnLeftDistance(.5,48);
+    robot.leftShooter.setPower(.436);
+    robot.rightShooter.setPower(-.436);
+    sleep(1000);
+    Shoot(.436,12000);
+    DriveBackwardDistance(.5,15);*/
+        sleep(100000000);
+
+
+    }}
+
+
+    public void TurnShootSlider() {
+
+
+        //robot.shootSlider.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+       // robot.shootSlider.setTargetPosition(robot.shootSlider.getCurrentPosition() + 113);
+
+        //robot.shootSlider.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        robot.shootSlider.setPower(1);
+
+        while (robot.shootSlider.isBusy()) {
+
+        }
+        //robot.shootSlider.setPower(0);
+        sleep(100);
+        robot.shootSlider.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+    }
+}
+    /*public void DriveBackwardDistance(double speed, double distanceInches)
+
+    {
+        double rotationsneeded = distanceInches/CIRCUMFERENCE;
+        int distanceTick = (int)(rotationsneeded*COUNTS_PER_MOTOR_REV);
+        robot.leftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.rightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.leftBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.rightBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        robot.leftDrive.setTargetPosition(robot.leftDrive.getCurrentPosition() + distanceTick);
+        robot.rightDrive.setTargetPosition(robot.rightDrive.getCurrentPosition() - distanceTick);
+        robot.leftBack.setTargetPosition(robot.leftBack.getCurrentPosition() + distanceTick);
+        robot.rightBack.setTargetPosition(robot.rightBack.getCurrentPosition() - distanceTick);
+
+        robot.leftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        robot.rightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        robot.leftBack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        robot.rightBack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        DriveBackward(speed);
+
+        while(robot.leftDrive.isBusy() && robot.rightDrive.isBusy() && robot.leftBack.isBusy() && robot.rightBack.isBusy() )
+        {
+
+        }
+        StopDriving();
+        sleep(100);
+        robot.leftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.rightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.leftBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.rightBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+    public void TurnLeftDistance(double speed, int distanceInches)
+    {
+        double rotationsneeded = distanceInches/CIRCUMFERENCE;
+        int distanceTick = (int)(rotationsneeded*COUNTS_PER_MOTOR_REV);
+        robot.rightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.rightBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.leftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.leftBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        robot.rightDrive.setTargetPosition(robot.rightDrive.getCurrentPosition() + distanceTick);
+        robot.rightBack.setTargetPosition(robot.rightBack.getCurrentPosition() + distanceTick);
+        robot.leftDrive.setTargetPosition(robot.leftDrive.getCurrentPosition() + distanceTick);
+        robot.leftBack.setTargetPosition(robot.leftBack.getCurrentPosition() + distanceTick);
+
+        robot.rightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        robot.rightBack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        robot.leftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        robot.leftBack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        TurnLeft(speed);
+
+
+        while( robot.rightDrive.isBusy() && robot.rightBack.isBusy() && robot.leftBack.isBusy() && robot.leftDrive.isBusy() )
+        {
+
+        }
+        StopDriving();
+        sleep(100);
+        robot.leftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.rightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.leftBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.rightBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+    public void TurnRightDistance(double speed, int distanceInches)
+    {
+        double rotationsneeded = distanceInches/CIRCUMFERENCE;
+        int distanceTick = (int)(rotationsneeded*COUNTS_PER_MOTOR_REV);
+        robot.leftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.leftBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.rightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.rightBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        robot.leftDrive.setTargetPosition(robot.leftDrive.getCurrentPosition() - distanceTick);
+        robot.leftBack.setTargetPosition(robot.leftBack.getCurrentPosition() - distanceTick);
+        robot.rightDrive.setTargetPosition(robot.rightDrive.getCurrentPosition() - distanceTick);
+        robot.rightBack.setTargetPosition(robot.rightBack.getCurrentPosition() - distanceTick);
+
+        robot.leftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        robot.leftBack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        robot.rightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        robot.rightBack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        TurnRight(speed);
+
+
+        while(robot.leftDrive.isBusy()  && robot.leftBack.isBusy() && robot.rightDrive.isBusy()  && robot.rightBack.isBusy() )
+        {
+
+        }
+        StopDriving();
+        sleep(100);
+        robot.leftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.rightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.leftBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.rightBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+    public void StrafeLeftDistance(double speed, int distanceInches)
+    {
+        double degree = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES).firstAngle;
+        double rotationsneeded = distanceInches/CIRCUMFERENCE;
+        int distanceTick = (int)(rotationsneeded*COUNTS_PER_MOTOR_REV);
+
+        robot.rightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.leftBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.rightBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.leftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+
+
+        robot.rightDrive.setTargetPosition(robot.rightDrive.getCurrentPosition() + distanceTick);
+        robot.leftBack.setTargetPosition(robot.leftBack.getCurrentPosition() - distanceTick);
+        robot.rightBack.setTargetPosition(robot.rightBack.getCurrentPosition() - distanceTick);
+        robot.leftDrive.setTargetPosition(robot.leftDrive.getCurrentPosition() + distanceTick);
+
+
+        robot.rightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        robot.leftBack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        robot.rightBack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        robot.leftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        StrafLeft(speed);
+        GyroCorrect(degree, imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES).firstAngle);
+        while(robot.rightDrive.isBusy() && robot.leftBack.isBusy() && robot.rightBack.isBusy() && robot.leftDrive.isBusy()) {
+
+        }
+
+        StopDriving();
+        sleep(100);
+        robot.leftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.rightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.leftBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.rightBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+    public void StrafeRightDistance(double speed, int distanceInches)
+    {
+        double rotationsneeded = distanceInches/CIRCUMFERENCE;
+        int distanceTick = (int)(rotationsneeded*COUNTS_PER_MOTOR_REV);
+
+        robot.leftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.rightBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.rightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.leftBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+
+
+        robot.rightBack.setTargetPosition(robot.rightBack.getCurrentPosition() + distanceTick);
+        robot.leftDrive.setTargetPosition(robot.leftDrive.getCurrentPosition() - distanceTick);
+        robot.rightDrive.setTargetPosition(robot.rightDrive.getCurrentPosition() - distanceTick);
+        robot.leftBack.setTargetPosition(robot.leftBack.getCurrentPosition() + distanceTick);
+
+
+
+        robot.rightBack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        robot.leftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        robot.rightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        robot.leftBack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+
+
+
+        StrafRight(speed);
+
+
+        while(robot.leftDrive.isBusy() && robot.rightBack.isBusy() && robot.leftBack.isBusy() && robot.rightDrive.isBusy())
+        {
+
+        }
+        StopDriving();
+        sleep(100);
+        robot.leftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.rightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.leftBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.rightBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+    public void RightDiagonal(double speed, double distanceInches)
+    {
+        double rotationsneeded = distanceInches/CIRCUMFERENCE;
+        int distanceTick = (int)(rotationsneeded*COUNTS_PER_MOTOR_REV);
+
+        robot.leftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.rightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.leftBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.rightBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        robot.leftDrive.setTargetPosition(robot.leftDrive.getCurrentPosition());
+        robot.rightDrive.setTargetPosition(robot.rightDrive.getCurrentPosition() + distanceTick);
+        robot.leftBack.setTargetPosition(robot.leftBack.getCurrentPosition() - distanceTick);
+        robot.rightBack.setTargetPosition(robot.rightBack.getCurrentPosition());
+
+        robot.leftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        robot.rightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        robot.leftBack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        robot.rightBack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        DriveForward(speed);
+
+        while(robot.leftDrive.isBusy() && robot.rightDrive.isBusy() && robot.leftBack.isBusy() && robot.rightBack.isBusy() )
+        {
+
+        }
+        StopDriving();
+        robot.leftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.rightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.leftBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.rightBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+    public void LeftDiagonal(double speed, double distanceInches)
+    {
+        double rotationsneeded = distanceInches/CIRCUMFERENCE;
+        int distanceTick = (int)(rotationsneeded*COUNTS_PER_MOTOR_REV);
+
+        robot.leftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.rightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.leftBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.rightBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        robot.leftDrive.setTargetPosition(robot.leftDrive.getCurrentPosition() - distanceTick);
+        robot.rightDrive.setTargetPosition(robot.rightDrive.getCurrentPosition());
+        robot.leftBack.setTargetPosition(robot.leftBack.getCurrentPosition());
+        robot.rightBack.setTargetPosition(robot.rightBack.getCurrentPosition() + distanceTick);
+
+        robot.leftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        robot.rightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        robot.leftBack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        robot.rightBack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        DriveForward(speed);
+
+        while(robot.leftDrive.isBusy() && robot.rightDrive.isBusy() && robot.leftBack.isBusy() && robot.rightBack.isBusy() )
+        {
+
+        }
+        StopDriving();
+        robot.leftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.rightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.leftBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.rightBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+    public void wobblyJoint(double power, int tick)
+    {
+        robot.wobblyJoint.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        robot.wobblyJoint.setTargetPosition(robot.wobblyJoint.getCurrentPosition() + tick);
+
+        robot.wobblyJoint.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        robot.wobblyJoint.setPower(power);
+
+        while (robot.wobblyJoint.isBusy())
+        {
+
+        }
+        robot.wobblyJoint.setPower(0);
+    }
+    public void DriveForward(double power)
+    {
+        robot.leftDrive.setPower(-power);
+        robot.rightDrive.setPower(power);
+        robot.rightBack.setPower(power);
+        robot.leftBack.setPower(-power);
+    }
+    public void DriveBackward(double power)
+    {
+        robot.leftDrive.setPower(-power);
+        robot.rightDrive.setPower(-power);
+        robot.rightBack.setPower(-power);
+        robot.leftBack.setPower(-power);
+    }
+    public void TurnLeft(double power)
+    {
+        robot.leftDrive.setPower(-power);
+        robot.rightDrive.setPower(power);
+        robot.rightBack.setPower(power);
+        robot.leftBack.setPower(-power);
+    }
+    public void TurnRight(double power)
+    {
+        robot.leftDrive.setPower(power);
+        robot.rightDrive.setPower(-power);
+        robot.rightBack.setPower(-power);
+        robot.leftBack.setPower(power);
+    }
+    public void StrafLeft(double power)
+    {
+        robot.leftDrive.setPower(-power);
+        robot.rightDrive.setPower(power);
+        robot.leftBack.setPower(power);
+        robot.rightBack.setPower(-power);
+    }
+    public void StrafRight(double power)
+    {
+        robot.leftDrive.setPower(power);
+        robot.rightDrive.setPower(-power);
+        robot.leftBack.setPower(-power);
+        robot.rightBack.setPower(power);
+    }
+    public void Shoot(double power, int sleepTime)
+    {
+        robot.leftShooter.setPower(power);
+        robot.rightShooter.setPower(-power);
+        robot.bottomSlider.setPower(power);
+        robot.topSlider.setPower(power);
+        sleep(sleepTime);
+        robot.leftShooter.setPower(0);
+        robot.rightShooter.setPower(0);
+        robot.bottomSlider.setPower(0);
+        robot.topSlider.setPower(0);
+        StopDriving();
+    }
+
+    public void StopDriving()
+    {
+        robot.leftDrive.setPower(0);
+        robot.rightDrive.setPower(0);
+        robot.leftBack.setPower(0);
+        robot.rightBack.setPower(0);
+    }
+    public void GyroCorrect(double degree1, double degree2){
+        if(degree1 < degree2){
+            while(imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES).firstAngle > degree1 + 0.5){
+                robot.leftDrive.setPower(-.1);
+                robot.rightDrive.setPower(-.1);
+                robot.leftBack.setPower(-.1);
+                robot.rightBack.setPower(-.1);
+            }
+        }
+        else if(degree1 > degree2){
+            while(imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES).firstAngle < degree1 - 0.5){
+                robot.leftDrive.setPower(.1);
+                robot.rightDrive.setPower(.1);
+                robot.leftBack.setPower(.1);
+                robot.rightBack.setPower(.1);
+            }
+        }
+
+
+
+    }
+}*/
---
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestHardwareBionicbot.java
@@ -0,0 +1,149 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+/**
+ * This is NOT an opmode.
+ *
+ * This class can be used to define all the specific hardware for a single robot.
+ * In this case that robot is a K9 robot.
+ *
+ * This hardware class assumes the following device names have been configured on the robot:
+ * Note:  All names are lower case and some have single spaces between words.
+ *
+ * Motor channel:  Left  drive motor:        "left_drive"
+ * Motor channel:  Right drive motor:        "right_drive"
+ *
+ * Motor channel:  Raising the claw:         "shoulder"
+ * Motor channel:  Raising the joint:        "elbow"
+ * Motor channel:  Pull mechanism:           "shaft"
+ *
+ *
+ * Servo channel:  Servo to raise/lower arm: "arm"
+ * Servo channel:  Servo to open/close claw: "claw"
+ *
+ * Note: the configuration of the servos is such that:
+ *   As the arm servo approaches 0, the arm position moves up (away from the floor).
+ *   As the claw servo approaches 0, the claw opens up (drops the game element).
+ */
+public class TestHardwareBionicbot
+{
+/* Public OpMode members. */
+public DcMotor leftBack = null;
+public DcMotor rightBack = null;
+public DcMotor leftDrive   = null;
+public DcMotor rightDrive  = null;
+/*public DcMotor intake  = null;
+public DcMotor leftShooter  = null;
+public DcMotor rightShooter  = null;
+public DcMotor wobblyJoint = null;
+public Servo wobblyClaw = null;
+public CRServo topSlider = null;
+public CRServo bottomSlider = null;
+public Servo distanceServo = null;
+public DistanceSensor distanceSensor = null;*/
+
+
+/*public DcMotor pincher = null;
+public DcMotor lifter = null;
+public DcMotor lifter1 = null;
+public DcMotor straight = null;
+public Servo Rightglock = null;
+public Servo Leftglock = null;*/
+//public ModernRoboticsI2cColorSensor LeftColorSensor  = null;
+//public ModernRoboticsI2cColorSensor BackColorSensor = null;
+//public ModernRoboticsI2cColorSensor FrontColorSensor = null;
+
+
+public final static double ARM_HOME = 0.0;
+public final static double MIN_ARM_RANGE = 0.0;
+public final static double MAX_ARM_RANGE = 1.0;
+
+//public DcMotor dustbin = null;
+public DcMotor[] motors = new DcMotor[4];
+//public Servo binBlock = null;
+//public AnalogInput pixy = null;
+//public GyroUnshafter gyro;
+//public ModernRoboticsI2cGyro gyro = null;
+
+HardwareMap hwMap  = null;
+private ElapsedTime period  = new ElapsedTime();
+
+/* Constructor */
+public TestHardwareBionicbot() {
+}
+
+/* Initialize standard Hardware interfaces */
+public void init(HardwareMap ahwMap) {
+    hwMap = ahwMap;
+    //Color_Sensor = hwMap.get(ColorSensor.class, "sensor_color");
+//    IntegratingGyroscope gyro;
+//    ModernRoboticsI2cGyro modernRoboticsI2cGyro;
+
+
+    leftDrive  = hwMap.get(DcMotor.class, "left_drive");
+    rightDrive = hwMap.get(DcMotor.class, "right_drive");
+    leftBack = hwMap.get(DcMotor.class, "left_back");
+    rightBack = hwMap.get(DcMotor.class, "right_back");
+    /*intake = hwMap.get(DcMotor.class, "intake");
+    leftShooter = hwMap.get(DcMotor.class, "left_shooter");
+    rightShooter = hwMap.get(DcMotor.class, "right_shooter");
+    wobblyJoint = hwMap.get(DcMotor.class, "wobbly_joint");
+    wobblyClaw = hwMap.get(Servo.class, "wobbly_claw");
+    topSlider = hwMap.get(CRServo.class,"top_slider");
+    bottomSlider = hwMap.get(CRServo.class,"bottom_slider");
+    distanceServo = hwMap.get(Servo.class,"distance_servo");
+    distanceSensor = hwMap.get(DistanceSensor.class,"distance_sensor");*/
+
+//    modernRoboticsI2cGyro = hwMap.get(ModernRoboticsI2cGyro.class, "gyro");
+//    gyro = (IntegratingGyroscope)modernRoboticsI2cGyro;
+    /*lifter = hwMap.get(DcMotor.class,"lifter");
+    pincher = hwMap.get(DcMotor.class, "pincher");
+    lifter1 = hwMap.get(DcMotor.class,"lifter1");
+    straight = hwMap.get(DcMotor.class,"straight");
+    Rightglock = hwMap.get(Servo.class, "Largeglock");
+    Leftglock = hwMap.get(Servo.class, "Smallglock");*/
+//    LeftColorSensor = hwMap.get(ModernRoboticsI2cColorSensor.class,"LeftColor");
+//    BackColorSensor = hwMap.get(ModernRoboticsI2cColorSensor.class, "BackColor");
+//    FrontColorSensor = hwMap.get(ModernRoboticsI2cColorSensor.class, "FrontColor");
+
+
+
+
+
+
+
+    //shaft1 = hwMap.get(DcMotor.class, "shaft1");
+    //shaft2 = hwMap.get(DcMotor.class, "shaft2");
+    //rake = hwMap.get(DcMotor.class, "rake");
+    //pixy = hwMap.get(AnalogInput.class, "pixy");
+
+    //grabberL = hwMap.get(Servo.class, "gripL");
+    //grabberR = hwMap.get(Servo.class, "gripR");
+    //tail = hwMap.get(Servo.class, "tail");
+    //gyro = new GyroUnshafter(hwMap.get(ModernRoboticsI2cGyro.class, "gyro"));
+  //  gyro = hwMap.get(ModernRoboticsI2cGyro.class, "gyro");
+    leftDrive.setDirection(DcMotor.Direction.REVERSE); // Set to REVERSE if using AndyMark motors
+    leftBack.setDirection(DcMotor.Direction.REVERSE);
+
+
+    motors[0] = rightDrive;
+    motors[1] = rightBack;
+    motors[2] = leftDrive;
+    motors[3] = leftBack;
+
+
+    //shaft1.setPower(0);
+    //shaft1.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+    //shaft2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    //rake.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+}
+}
